69bcde37bc05ae71e7ee929d3fc41b913673c252
Simándi Barbara <barbara.simandi@itron.com>
2022-08-29 23:51:35 +0200

    Install axis, and fetch data from backend

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import React from "react";
-import { movies_sample } from "./movies_sample";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import MovieCardList from "./components/MovieCardList";
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 
 function App() {
-  const movies = movies_sample
+  const [movies, setMovies] = useState([])
+
+  const getMovies = async () => {
+    try {
+      const { data } = await axios.get(`http://localhost:4000/api/v1/movies?search=The`);
+      setMovies(data.data);
+    } catch (error) {
+      console.log(error)
+    }
+  };
+
+  useEffect(() => {
+    getMovies();
+  }, [])
 
   return (
     <div className="App">
