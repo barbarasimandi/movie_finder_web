18bd9f0a7dabf67fe52840d89085f5b891d0777c
Simándi Barbara <barbara.simandi@itron.com>
2022-08-30 00:19:11 +0200

    Add pagination
    	- use styled Pagination component from material ui
    	- setup pages states

--- src/App.js
+++ src/App.js
@@ -2,17 +2,21 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import SearchForm from './components/SearchForm'
 import MovieCardList from "./components/MovieCardList";
+import Pagination from '@mui/material/Pagination';
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 
 function App() {
   const [movies, setMovies] = useState([])
   const [search, setSearch] = useState('')
+  const [totalPages, setTotalPages] = useState(1)
+  const [currentPage, setCurrentPage] = useState(0)
 
   const getMovies = async () => {
     try {
-      const { data } = await axios.get(`http://localhost:4000/api/v1/movies?search=${search}`);
+      const { data } = await axios.get(`http://localhost:4000/api/v1/movies?search=${search}&page=${currentPage}`);
       setMovies(data.data);
+      setTotalPages(data.total_pages)
     } catch (error) {
       console.log(error)
     }
@@ -20,10 +24,15 @@ function App() {
 
   useEffect(() => {
     getMovies();
-  }, [search])
+  }, [search, currentPage])
 
   const searchHandler = (query) => {
     setSearch(query)
+  }
+
+  const paginationHandler = (event, currentPage) => {
+    event.preventDefault();
+    setCurrentPage(currentPage)
   }
 
   return (
@@ -34,6 +43,13 @@ function App() {
       <MovieCardList
         movies={movies}
       />
+      {totalPages > 1 &&
+      <Pagination
+        showFirstButton={true}
+        showLastButton={true}
+        count={totalPages}
+        onChange={paginationHandler}
+      />}
     </div>
   );
 }
